8c46f4a0d1b627b74647e4f051df0474ac6f9df9
Diego Auza <auza0002@algonquinlive.com>
2023-12-05 22:09:09 -0500

    set initState to get movies when open the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:movie_night_flutter/screen_controller.dart';
+import 'package:movie_night_flutter/utils/http_helper_tmdb.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    HTTPHelperTMDB().getDataPrimaryList();
+  }
 
   @override
   Widget build(BuildContext context) {
