0bbfbdd71ebdab98f41b7d53a7ef00e4af55ebf4
Diego Auza <auza0002@algonquinlive.com>
2023-12-06 10:02:22 -0500

    Fixing primaryStrcut

--- lib/model/primary.dart
+++ lib/model/primary.dart
@@ -1,15 +1,23 @@
-class Primary {
+class Movies {
   int page;
   List<Result> results;
   int totalPages;
   int totalResults;
 
-  Primary({
+  Movies({
     required this.page,
     required this.results,
     required this.totalPages,
     required this.totalResults,
   });
+
+  factory Movies.fromJson(Map<String, dynamic> json) => Movies(
+        page: json["page"],
+        results:
+            List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+      );
 }
 
 class Result {
@@ -44,4 +52,21 @@ class Result {
     required this.voteAverage,
     required this.voteCount,
   });
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"]?.toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: DateTime.parse(json["release_date"]),
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"]?.toDouble(),
+        voteCount: json["vote_count"],
+      );
 }
--- lib/utils/http_helper_tmdb.dart
+++ lib/utils/http_helper_tmdb.dart
@@ -4,28 +4,26 @@ import 'package:http/http.dart';
 import 'dart:convert';
 import 'dart:async';
 
-class HTTPHelperTMDB {
-  // get the upcoming movies, 20 at the time.
-  // get the best anime moives, 10 at the time.
-  // get the best anime series, 10 at the time.
+import 'package:movie_night_flutter/model/primary.dart';
 
+class HTTPHelperTMDB {
   final uri = Uri.parse(
       'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1');
-
-  final primaryList = [];
   final topMovies = [];
   final topTV = [];
 
-  final _myKey =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWEyMTM3M2ZmNGEzOGQ3NTliM2UxZmRkNGI0YzA5ZiIsInN1YiI6IjYzOGY2YmNmMDQ3MzNmMDA4NWQwYWFkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MoHcQDxp_anG4kiYxxLgZ4hywHvE670dDFZcP6ixJ_k";
-
-  void getDataPrimaryList() async {
+  static Future<List<Movies>> getDataMovieHomeScreen(
+      String url, String key) async {
+    Uri uri = Uri.parse(url);
+    String myKey = key;
     Response response = await get(uri, headers: {
-      HttpHeaders.authorizationHeader: 'Authorization: Bearer $_myKey',
+      HttpHeaders.authorizationHeader: 'Authorization: Bearer $myKey',
       HttpHeaders.contentTypeHeader: 'application/json',
     });
     if (response.statusCode == 200) {
-      print("good response");
+      Map<String, dynamic> data = jsonDecode(response.body);
+      var movies = Movies.fromJson(data);
+      return [movies];
     } else {
       throw Exception("Did not get a valid response");
     }
