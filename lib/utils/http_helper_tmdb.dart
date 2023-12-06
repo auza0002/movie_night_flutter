375df38ae125a7c6a083d74c02f2265ae62c8873
Diego Auza <auza0002@algonquinlive.com>
2023-12-05 21:13:12 -0500

    adding HTPP to the flutter app

---
+++ lib/utils/http_helper_tmdb.dart
@@ -0,0 +1,7 @@
+class HTTPHelperTMDB {
+  // get the upcoming movies, 20 at the time.
+  // get the best anime moives, 10 at the time.
+  // get the best anime series, 10 at the time.
+  final _myKey =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWEyMTM3M2ZmNGEzOGQ3NTliM2UxZmRkNGI0YzA5ZiIsInN1YiI6IjYzOGY2YmNmMDQ3MzNmMDA4NWQwYWFkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MoHcQDxp_anG4kiYxxLgZ4hywHvE670dDFZcP6ixJ_k";
+}
