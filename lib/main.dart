b89e4c24c59bb30304b8546497739eda22851eef
Diego Auza <auza0002@algonquinlive.com>
2023-12-06 10:02:08 -0500

    List of Movies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_night_flutter/model/primary.dart';
 import 'package:movie_night_flutter/screen_controller.dart';
 import 'package:movie_night_flutter/utils/http_helper_tmdb.dart';
 
@@ -14,10 +15,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Movies> movies = [];
+
+  final _myKey =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWEyMTM3M2ZmNGEzOGQ3NTliM2UxZmRkNGI0YzA5ZiIsInN1YiI6IjYzOGY2YmNmMDQ3MzNmMDA4NWQwYWFkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MoHcQDxp_anG4kiYxxLgZ4hywHvE670dDFZcP6ixJ_k";
+  final url =
+      'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+
+  void getDataHomeScreen() async {
+    List<Movies> movies =
+        await HTTPHelperTMDB.getDataMovieHomeScreen(url, _myKey);
+    setState(() {
+      this.movies = movies;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    HTTPHelperTMDB().getDataPrimaryList();
+    getDataHomeScreen();
   }
 
   @override
