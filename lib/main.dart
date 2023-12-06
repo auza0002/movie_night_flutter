3984893ee2d0e4562e345a6743933f0130836d4f
Diego Auza <auza0002@algonquinlive.com>
2023-12-06 11:03:02 -0500

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:movie_night_flutter/model/primary.dart';
+import 'package:movie_night_flutter/providers/movives_home_provider.dart';
 import 'package:movie_night_flutter/screen_controller.dart';
-import 'package:movie_night_flutter/utils/http_helper_tmdb.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => MoviesProvider()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -15,25 +17,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Movies> movies = [];
-
-  final _myKey =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMWEyMTM3M2ZmNGEzOGQ3NTliM2UxZmRkNGI0YzA5ZiIsInN1YiI6IjYzOGY2YmNmMDQ3MzNmMDA4NWQwYWFkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MoHcQDxp_anG4kiYxxLgZ4hywHvE670dDFZcP6ixJ_k";
-  final url =
-      'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-
-  void getDataHomeScreen() async {
-    List<Movies> movies =
-        await HTTPHelperTMDB.getDataMovieHomeScreen(url, _myKey);
-    setState(() {
-      this.movies = movies;
-    });
-  }
-
   @override
   void initState() {
     super.initState();
-    getDataHomeScreen();
+    context.read<MoviesProvider>().getDataHomeScreen();
   }
 
   @override
