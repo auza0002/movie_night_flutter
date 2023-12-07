f7bc5d9cdb91cb2be291af1c0103409b3a1cddea
Diego Auza <auza0002@algonquinlive.com>
2023-12-07 18:09:07 -0500

    chaning android

--- lib/providers/game_provider.dart
+++ lib/providers/game_provider.dart
@@ -1,15 +1,19 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/cupertino.dart';
 import 'package:movie_night_flutter/utils/http_helper_movie_ninght.dart';
 
 class GameProvider with ChangeNotifier {
   String _myDeviceID = '';
   String _myKey = '';
-  String _url = '';
+//start-session?device_id=123
+
+  String get getMyDeviceID => _myDeviceID;
+  String get getMyKey => _myKey;
 
   void getDeviceID() async {
     var deviceID = await HTTPHelperMovieNight().initPlatformState();
     _myDeviceID = deviceID;
-    print('Device ID: $_myDeviceID');
     notifyListeners();
   }
 }
--- lib/utils/http_helper_movie_ninght.dart
+++ lib/utils/http_helper_movie_ninght.dart
@@ -1,12 +1,19 @@
-import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
 import 'dart:async';
 
 import 'package:platform_device_id/platform_device_id.dart';
 
 class HTTPHelperMovieNight {
+  Future<Map> getMySessionID(String deviceID) async {
+    String _url =
+        'https://movie-night-api.onrender.com/start-session?device_id=$deviceID';
+    Map data = {
+      "data": {"message": "error", "code": "0000", "session_id": "null"}
+    };
+
+    return data;
+  }
+
   Future<String> initPlatformState() async {
     String? deviceId;
     try {
